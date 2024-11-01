ad84c12ed7af20779228cfc79d62ce8fd4c5a724
Aruthra S <aruthras06@gmail.com>
2024-11-02 00:49:41 +0530

    [fix] removed testRouter 🔧

--- AruthraS/server.py
+++ AruthraS/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers.sampleRouter.router import router as testRouter
+# from routers.sampleRouter.router import router as testRouter
 from routers.BotRouter.router import router as botRouter
 from routers.AuthRouter.router import router as authRouter
 
@@ -14,7 +14,7 @@ app.add_middleware(
     # allow_credentials=True 
 )
 
-app.include_router(testRouter)
+# app.include_router(testRouter)
 app.include_router(botRouter,prefix="/bot")
 app.include_router(authRouter, prefix="/auth")
 
